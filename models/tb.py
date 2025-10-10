3829b085105ac64dbdef0c24a69cd162b5564224
Edward0333 <876166923@qq.com>
2025-10-10 15:52:02 +0800

    first commit

---
+++ models/tb.py
@@ -0,0 +1,56 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TB(pl.LightningModule):
+    def __init__(self,
+                 historical_steps: int,
+                 future_steps: int,
+                 num_modes: int,
+                 rotate: bool,
+                 node_dim: int,
+                 edge_dim: int,
+                 embed_dim: int,
+                 num_heads: int,
+                 dropout: float,
+                 num_temporal_layers: int,
+                 num_global_layers: int,
+                 local_radius: float,
+                 parallel: bool,
+                 lr: float,
+                 weight_decay: float,
+                 T_max: int,
+                 **kwargs):
+        super(TB, self).__init__()
+    def forward(self,data):
+        pass
+    def training_step(self, batch, batch_idx):
+        pass
+    def validation_step(self, batch, batch_idx):
+        pass
+
+    def configure_optimizers(self):
+        pass
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group('HiVT')
+        parser.add_argument('--historical_steps', type=int, default=20)
+        parser.add_argument('--future_steps', type=int, default=30)
+        parser.add_argument('--num_modes', type=int, default=6)
+        parser.add_argument('--rotate', type=bool, default=True)
+        parser.add_argument('--node_dim', type=int, default=2)
+        parser.add_argument('--edge_dim', type=int, default=2)
+        parser.add_argument('--embed_dim', type=int, default=64)
+        parser.add_argument('--num_heads', type=int, default=8)
+        parser.add_argument('--dropout', type=float, default=0.1)
+        parser.add_argument('--num_temporal_layers', type=int, default=4)
+        parser.add_argument('--num_global_layers', type=int, default=3)
+        parser.add_argument('--local_radius', type=float, default=50)
+        parser.add_argument('--parallel', type=bool, default=False)
+        parser.add_argument('--lr', type=float, default=5e-4)
+        parser.add_argument('--weight_decay', type=float, default=1e-4)
+        parser.add_argument('--T_max', type=int, default=64)
+        return parent_parser
